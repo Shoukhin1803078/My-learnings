fc141ea6be4bc0ee0deed3c83a703e30ac5b6ebc
Shoukhin1803078 <alamintokdercse@gmail.com>
2025-06-14 16:32:39 +0600

    fastapi easily cse learn channel tutorial practice code

---
+++ Fast_Api_Learning/Easily_CSE_Learn/12_path_parameter_with_dropdown/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app=FastAPI()
+
+class ModelName(str,Enum):
+    resnet="resnet"
+    efficientnet="efficientnet"
+    vgg16="vgg16"
+    alexnet="alexnet"
+    densenet="densenet"
+    mobilenet="mobilenet"
+
+
+@app.get("/model/{model_name}")
+def get_model(model_name:ModelName):
+    if model_name==ModelName.resnet:
+        return{
+            "model_name":model_name,
+            "message":"You have selected ResNet model"
+        }
+    elif model_name==ModelName.efficientnet:
+        return{
+            "model_name":model_name,
+            "message":"You have selected EfficientNet model"
+        }
+    elif model_name==ModelName.vgg16:
+        return{
+            "model_name":model_name,
+            "message":"You have selected VGG16 model"
+        }
+    elif model_name==ModelName.alexnet:
+        return{
+            "model_name":model_name,
+            "message":"You have selected AlexNet model"
+        }
+    elif model_name==ModelName.densenet:
+        return{
+            "model_name":model_name,
+            "message":"You have selected DenseNet model"
+        }
+    elif model_name==ModelName.mobilenet:
+        return{
+            "model_name":model_name,
+            "message":"You have selected MobileNet model"
+        }
+    else:
+        return{
+            "model_name":"No model selected",
+            "message":"Please select a valid model"
+        }
