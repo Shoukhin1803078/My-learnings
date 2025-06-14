3c6ade4a43067d32bd275bf060c843255602b0c5
Shoukhin1803078 <alamintokdercse@gmail.com>
2025-06-14 16:34:56 +0600

    fastapi learning updated

--- Fast_Api_Learning/1_introduction/main.py
+++ Fast_Api_Learning/1_introduction/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
+
+# @app.get("/")
+# async def read_root():
+#     return {
+#         "message": "Welcome to FastAPI Learning!"
+#     }
+
+from fastapi import FastAPI
+app= FastAPI(title="FastAPI Introduction",description="A simple FastAPI application to demonstrate basic features.",version="1.0.0")
 
 @app.get("/")
-async def read_root():
-    return {
-        "message": "Welcome to FastAPI Learning!"
-    }
+def root():
+    return {"hello":"hello world"}
---
+++ Fast_Api_Learning/Easily_CSE_Learn/8_bookstore_create_and_show/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+app=FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {
+        "message": "Welcome to FastAPI Learning!"
+    }
+
+@app.get("/book_id/{book_id}")
+async def read_root(book_id:int):
+    return {
+        "Book_ID":book_id,
+        "title":"FastAPI Learning",
+        "author":"Alamin",
+    }
